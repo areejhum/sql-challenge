50a2bcc34cd6dbe73a5a59a9253f56d06de5bd37
areejhum <63086810+areejhum@users.noreply.github.com>
2020-06-28 04:40:57 -0500

    Update Table Schemata.sql

--- EmployeeSQL/Table Schemata.sql
+++ EmployeeSQL/Table Schemata.sql
@@ -1,1 +1,60 @@
+--Data Modeling
 
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+ drop table dept_emp
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL
+    
+);
+
+select * from dept_emp
+
+drop table dept_manager
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INT   NOT NULL
+    
+);
+
+select * from dept_manager
+
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+	"emp_title_id" VARCHAR NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+select * from employees
+
+drop table salaries
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+    
+);
+
+select * from salaries
+
+DROP TABLE titles
+CREATE TABLE "titles" (
+    
+    "title" VARCHAR   NOT NULL,
+    "title_id" VARCHAR   NOT NULL
+    
+);
+select * from titles
